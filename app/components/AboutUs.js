fd9f2b09c2d8cc0ae6b2ad751b081aec030b9fd6
Tilinger <vmmc100@gmail.com>
2025-06-09 15:34:09 -0400

    Prueba

--- app/components/AboutUs.js
+++ app/components/AboutUs.js
@@ -1,22 +1,7 @@
 export default function AboutUs () {
     return (
         <div className="my-4 flex flex-col text-black">
-            <div className="flex my-4">
-            <h2 id="about-us" className="text-3xl font-bold  ">Quienes somos</h2>
-            </div >
-            <div className="flex flex-col align-middle ">
-            <p className="text-xl ">
-                <b>PIXELWORLD</b> es el nombre de fantasia de
-                nuestra empresa <b>Paty y Ángel inversiones SPA</b>.
-            </p>
-            <br />
-            <p className="text-xl ">
-                <b>Nos dedicamos al entretenimiento</b> y
-                disfrute por medio de renta de consolas y
-                equipos que permiten transportar a una
-                nueva experiencia de juego.
-            </p>
-            </div>
+
         </div>
     )
 }
