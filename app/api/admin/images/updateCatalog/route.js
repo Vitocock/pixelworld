62447badf6c617178735f6d711573a281b98ffc8
Tilinger <vmmc100@gmail.com>
2025-07-17 20:24:35 -0400

    Varaibles de entorno corregidas

--- app/api/admin/images/updateCatalog/route.js
+++ app/api/admin/images/updateCatalog/route.js
@@ -7,8 +7,8 @@ import { requireAdmin } from '@/lib/requireAdmin'
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
+    accessKeyId: process.env.AWS_ACCESS,
+    secretAccessKey: process.env.AWS_SECRET
   }
 })
 
