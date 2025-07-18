906da47260267eed8a081dd5592f70f9132f7085
Tilinger <vmmc100@gmail.com>
2025-07-18 17:34:56 -0400

    Ahora todo el catalogo se puede pulsar

--- app/components/Catalog.js
+++ app/components/Catalog.js
@@ -3,7 +3,10 @@
 export default function Catalog({catalogUrl}) {
  
   return (
-    <section className="h-[36rem] lg:h-dvh py-12 grid grid-cols-4 sm:grid-cols-5 grid-rows-1 overflow-hidden">
+    <a 
+      href={catalogUrl}
+      target="_blank"
+      className="h-[36rem] lg:h-dvh py-12 grid grid-cols-4 sm:grid-cols-5 grid-rows-1 overflow-hidden">
       <img className="col-span-2 object-cover w-full h-full transform hover:scale-105 hover:z-10 transition-all duration-300" src="./Catalog-Pic1(FC25).jpg" />
       <div className="col-span-2 hover:z-10">
         <img className="object-cover object-top w-full h-1/2 transform hover:scale-105 transition-all duration-300" src="./Catalog-Pic2(Halo).jpg" />
@@ -15,19 +18,14 @@ export default function Catalog({catalogUrl}) {
         <img className="object-cover object-top w-full h-1/3 transform hover:scale-105 transition-all duration-300" src="./Catalog-Pic6(RL).jpg" />
       </div>
 
-      {/* Link al catálogo con URL dinámica */}
-      {catalogUrl && (
-        <p className="mb-4 absolute flex justify-self-center self-end transform hover:scale-105 transition-all duration-300 z-20">
-          <a
-            className="text-center text-white text-3xl lg:text-5xl font-bold font-['Orbitron'] [text-shadow:_0px_0px_65px_rgb(0_0_0_/_1.00)]"
-            href={catalogUrl}
-            target="_blank"
-          >
-            Haz click para ver nuestro catálogo de juegos
-          </a>
+      <div className="bg-black bg-opacity-20 px-2 mb-4 absolute flex justify-self-center self-end transform hover:scale-105 transition-all duration-300 z-20">
+        <p
+          className="text-center text-white text-3xl lg:text-5xl font-bold font-['Orbitron'] [text-shadow:_0px_0px_65px_rgb(0_0_0_/_1.00)]"
+        >
+          Haz click para ver nuestro catálogo de juegos
         </p>
-      )
-    }
-    </section>
+      </div>
+
+    </a>
 )
 }
