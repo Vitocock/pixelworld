972a1ff5a4fcd612ec730fbde0900d4f6bf0bc44
ElPochero <cvivar@utem.cl>
2025-07-18 21:21:43 -0400

    Productos scrolleables(movil)

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,12 +1,12 @@
 export default function ProductCard ({Product}){
-    const {name, brand, base_price, image} = Product
+    const {name, image} = Product
 
     return(
-        <a className="w-full xl:w-1/3 h-64 sm:h-full hover:scale-105 transform transition-all duration-300 overflow-hidden" href="/products" target="_blank">
+        <a className="w-full h-64 sm:h-full transform overflow-hidden" href="/products" target="_blank">
             <img
             src={image}
             alt={`${name} image`}
-            className="w-full h-full object-cover will-change-transform"
+            className="w-full h-full object-cover hover:scale-105 will-change-transform transition-all duration-300"
         />
         </a>
     )
