936b4ab1f1881deb21b21d8a9de404778c2bfd47
Tilinger <vmmc100@gmail.com>
2025-07-18 22:42:14 -0400

    correccion strokeWidth

--- app/components/TextCarousel.js
+++ app/components/TextCarousel.js
@@ -25,7 +25,7 @@ export default function TextCarousel({ items, interval }) {
 
   const Arrow = () =>
     <svg width="16px" height="22px" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
-      <path fill="none" stroke="#ffffff" stroke-width="2" d="M3,22.0000002 L21,12 L3,2 L3,22.0000002 Z M5,19 L17.5999998,11.9999999 L5,5 L5,19 Z M7,16 L14.1999999,12 L7,8 L7,16 Z M9,13 L10.8,12 L9,11 L9,13 Z"></path>
+      <path fill="none" stroke="#ffffff" strokeWidth="2" d="M3,22.0000002 L21,12 L3,2 L3,22.0000002 Z M5,19 L17.5999998,11.9999999 L5,5 L5,19 Z M7,16 L14.1999999,12 L7,8 L7,16 Z M9,13 L10.8,12 L9,11 L9,13 Z"></path>
     </svg>
   return (
     <div className="w-full overflow-hidden h-60 text-white rounded-lg shadow-md">
